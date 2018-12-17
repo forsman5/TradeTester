3f29d56fa92a9b7cc203dcb30cefbe1f552b9c9c
forsman5 <jrforsman@gmail.com>
2018-12-16 22:11:31 -0500

    basic ejs sculpting

--- webapp/main.js
+++ webapp/main.js
@@ -1,6 +1,23 @@
 var http = require('http');
+var path = require('path');
+const express = require('express')
+const app = express()
+const port = 8080
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('zach i have christmas presents for you, do you want them?');
-}).listen(8080);
+//point all requests for static resources to the resources folder automatically
+app.use(express.static(path.join(__dirname, '/resources')));
+
+app.set('view engine', 'ejs');
+
+// routes
+app.get('/', function (req, res) {
+  res.render('pages/index', {});
+});
+
+app.post('/', function (req, res) {
+  // TODO
+});
+
+app.listen(port, function () {
+   console.log("listening on " + port);
+});
